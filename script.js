2da3accb5b9afaba4fd8a9b88db2fe6a90666217
VanMan119 <StoneE5936@gmail.com>
2026-01-17 20:07:32 -0600

    Updating script to display data hopefully

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ fetch("data.json")
     .then(response => response.json())
     .then(data => {
         console.log(data);
+
+        const list = document.getElementById("top-artists-total");
+
+        data.total.artists.forEach(artist => {
+            const li = document.createElement("li");
+
+            li.textContent = `${artist.name} - ${artist.minutes.toFixed(1)} min (${artist.plays} plays)`;
+
+            list.appendChild(li);
+        })
     });
